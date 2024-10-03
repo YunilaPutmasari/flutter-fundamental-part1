9c5647dbbd7bbab04db2fc507b728441ec3b88bf
YunilaPutmasari <nilaputma26@gmail.com>
2024-10-03 10:23:58 +0700

    praktikum

--- lib/basic_widgets/loading_cupertino.dart
+++ lib/basic_widgets/loading_cupertino.dart
@@ -4,21 +4,22 @@ import 'package:flutter/cupertino.dart'; // Untuk menggunakan Cupertino widgets
 class LoadingCupertino extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Container(
-        margin: const EdgeInsets.only(top: 30),
-        color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            CupertinoButton(
-              child: const Text("Contoh button"),
-              onPressed: () {
-                // Aksi ketika button ditekan
-              },
-            ),
-            const CupertinoActivityIndicator(), // Loading bar gaya iOS
-          ],
-        ),
+    return Container(
+      margin: const EdgeInsets.only(top: 30),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Menengahkan widget secara vertikal
+        children: <Widget>[
+          CupertinoButton(
+            child: const Text("button"),
+            onPressed: () {
+              // Aksi ketika button ditekan
+              print("Button ditekan");
+            },
+          ),
+          const SizedBox(height: 20), // Memberikan jarak antara button dan loading
+          const CupertinoActivityIndicator(), // Loading bar gaya iOS
+        ],
       ),
     );
   }
